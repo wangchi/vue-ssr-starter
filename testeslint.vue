351bb865b7600e3d89e32cb0780e6dfe9be2b861
Wang Chi <hiwangchi@gmail.com>
2019-02-20 23:55:57 +0800

    添加eslint 并测试 npm run lint

---
+++ testeslint.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    hello {{ message }}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: 'meet vue'
+    };
+  },
+
+  methods: {
+    test() {
+      return 1;
+    }
+  }
+};
+</script>
